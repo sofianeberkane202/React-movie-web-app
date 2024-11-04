901d45f812ca6ca38580a78b466c95983687623f
sofiane berkane <sofianeberkane202@gmail.com>
2024-11-04 21:40:20 +0100

    finish stup css envirenement

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,29 @@
+import '../css/componentCSS/Header.css'
 export default function Header(){
-    return <h1>Header</h1>
+    return (
+        <header className="header">
+            <div className="container flex flex-center-y flex-between">
+                {/* logo */}
+                <div className="logo">
+                    <span className="text-header">🍿 usePopcorn</span>
+                </div>
+
+                {/* search field */}
+
+                <div className="box-search-field">
+                    <input 
+                    className='search-field text-field flex-1'
+                    type='text'
+                    placeholder='Search movies...'/>
+                </div>
+                
+
+                {/* count result */}
+    
+                <p className="count-movies text-field">Found 20 results</p>
+
+            </div>
+            
+        </header>
+    )
 }
