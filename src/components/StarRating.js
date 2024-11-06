540789486b65b68fe48f6c3f97771b9d5fba66e7
sofiane berkane <sofianeberkane202@gmail.com>
2024-11-06 14:10:12 +0100

    finish building the UI of a standard  star component

---
+++ src/components/StarRating.js
@@ -0,0 +1,65 @@
+export default function StarRating({
+    maxRate=5,
+    color='blue',
+    size=24,
+    starClassName='',
+    boxClassName,
+    textClassName,
+    starIsfilled= false 
+}){
+
+    const starStyle= {
+        width: `${size}px`,
+        maxRate: maxRate
+    }
+
+    return (
+        
+            <div 
+            className={`flex flex-center-y ${boxClassName}`}
+            style={{gap: '2px'}}
+            >    
+
+                {
+                    Array.from({length:maxRate}, (_,i) => i).
+                    map(i => {
+                        return (
+                            <span 
+                            className={`star ${starClassName}`} 
+                            style={starStyle}
+                            key={`star${i}`}
+                            > 
+                                <svg 
+                                xmlns="http://www.w3.org/2000/svg" 
+                                fill={starIsfilled ? color :"none"} viewBox="0 0 24 24" 
+                                stroke={color}
+                        
+                                >
+                                    <path 
+                                    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1
+                                    0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1
+                                    1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538
+                                    1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976
+                                    2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0
+                                    00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0
+                                    00.951-.69l1.519-4.674z" strokeLinecap="round" strokeLinejoin="round"
+                                    strokeWidth="{2}">
+                                    </path>
+                                </svg>
+                            </span>
+                        )
+                    }) 
+                }
+                                
+                <p 
+                className={textClassName}
+                style={{color:color}}
+                >
+                    0
+                </p>
+            </div>
+
+            
+        
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import StarRating from './components/StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Test/>
   </React.StrictMode>
 );
+
+function Test(){
+  return (
+    <StarRating
+    maxRate={10}
+    size={24}
+    color='#fcc419'
+    starClassName=''
+    boxClassName='starRating'
+    starIsfilled= {true}
+    textClassName='text text-btn'
+    />
+  )
+}
