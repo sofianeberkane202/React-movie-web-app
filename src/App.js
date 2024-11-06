0edf9f77bff7fd9019b9cc28ca34861f085ce91e
sofiane berkane <sofianeberkane202@gmail.com>
2024-11-07 00:36:57 +0100

    add feature returning on wached list movies

--- src/App.js
+++ src/App.js
@@ -270,14 +270,30 @@ export default function App(){
                     onRate={setRate}
                     rate={rate}
                     />
-                    <Btn text='+Add to List'/>
+                    <Btn 
+                    className='btn btn-primary text-btn' 
+                    text='+Add to List'/>
 
               </StarRatingBox>
               
               <MovieStory Plot={movieDetails.Plot}/>
 
-              <Btn text='Details'/>
+              <Btn 
+              className='btn btn-primary text-btn'
+              text='Details'/>
             </Details>
+
+            <Btn 
+            className='return-btn'
+            onMovieID={setMovieID}
+            onMovieDetails={setMovieDetails}
+            >
+              <svg 
+                xmlns="http://www.w3.org/2000/svg"
+                height="32px" viewBox="0 -960 960 960"
+                width="32px" fill="#000">
+                <path d="m480-320 56-56-64-64h168v-80H472l64-64-56-56-160 160 160 160Zm0 240q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+            </Btn>
 
           </MovieDetails>
           
