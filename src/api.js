4c7e7f36f65d649178af1df1b4f2cb787caef907
sofiane berkane <sofianeberkane202@gmail.com>
2024-11-07 19:39:49 +0100

    update API.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 const KEY = '1426b0e';
-const URLBySearch= `http://www.omdbapi.com/?apikey=${KEY}&s=`;
-const URLByID = `http://www.omdbapi.com/?apikey=${KEY}&i=`;
+const URLBySearch= `https://www.omdbapi.com/?apikey=${KEY}&s=`;
+const URLByID = `https://www.omdbapi.com/?apikey=${KEY}&i=`;
 export {KEY,URLByID,URLBySearch}
